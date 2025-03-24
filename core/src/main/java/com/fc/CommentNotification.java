460afe2b1303853067fa9a9c9ba12e8a2765d8c3
CHOHUNE <yuureru@gmail.com>
2025-03-25 00:02:48 +0900

    feat : CommentNotification 객체 commentId 지역변수 추가, Swagger Spec 추가, Controller 추가

--- core/src/main/java/com/fc/CommentNotification.java
+++ core/src/main/java/com/fc/CommentNotification.java
@@ -1,6 +1,5 @@
 package com.fc;
 
-import com.fc.config.NotificationType;
 import java.time.Instant;
 import lombok.Getter;
 import org.springframework.data.annotation.TypeAlias;
@@ -12,14 +11,16 @@ public class CommentNotification extends Notification {
     private final Long postId;
     private final Long writerId;
     private final String comment;
+    private final Long commentId;
 
     public CommentNotification(String id, Long userId, NotificationType type,
         Instant createdAt, Instant occurredAt, Instant lastUpdatedAt,
-        Instant deletedAt, Long postId, Long writerId, String comment) {
+        Instant deletedAt, Long postId, Long writerId, String comment, Long commentId) {
         super(id, userId, type, createdAt, occurredAt, lastUpdatedAt, deletedAt);
         this.postId = postId;
         this.writerId = writerId;
         this.comment = comment;
+        this.commentId = commentId;
     }
 
 
