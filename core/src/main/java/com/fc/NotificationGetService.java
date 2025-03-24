c5204c520fe197e23fe2caab107e1792dbc4aeff
CHOHUNE <yuureru@gmail.com>
2025-03-24 23:59:26 +0900

    feat : service 추가 ( NotificationGetService, NotificationSaveService, NotificationSaveService )

---
+++ core/src/main/java/com/fc/NotificationGetService.java
@@ -0,0 +1,21 @@
+package com.fc;
+
+
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class NotificationGetService {
+
+    @Autowired
+    private NotificationRepository repository;
+
+    public Optional<Notification> getNotification(NotificationType type , Long commentId) {
+
+        return repository.findByTypeAndCommentId(type, commentId);
+    }
+
+}
