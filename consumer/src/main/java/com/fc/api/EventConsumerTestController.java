bc5c57e10972d8bdd24323e48404eb04e8f9725f
CHOHUNE <yuureru@gmail.com>
2025-03-26 01:12:41 +0900

    feat : consumer test Like controller, spec update

--- consumer/src/main/java/com/fc/api/EventConsumerTestController.java
+++ consumer/src/main/java/com/fc/api/EventConsumerTestController.java
@@ -2,22 +2,34 @@ package com.fc.api;
 
 
 import com.fc.event.CommentEvent;
+import com.fc.event.LikeEvent;
 import java.util.function.Consumer;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class EventConsumerTestController implements EventConsumerTestControllerSpec{
 
-    @Autowired
-    private Consumer<CommentEvent> comment;
+
+
+    private final Consumer<CommentEvent> comment;
+    private final Consumer<LikeEvent> like;
+
+
 
     @Override
     @PostMapping("/test/comment")
     public void comment(@RequestBody  CommentEvent event) {
         comment.accept(event);
+    }
+
+    @Override
+    @PostMapping("/test/like")
+    public void like(@RequestBody LikeEvent event) {
+        like.accept(event);
     }
 
 }
--- consumer/src/main/java/com/fc/api/EventConsumerTestControllerSpec.java
+++ consumer/src/main/java/com/fc/api/EventConsumerTestControllerSpec.java
@@ -1,12 +1,14 @@
 package com.fc.api;
 
 import com.fc.event.CommentEvent;
+import com.fc.event.LikeEvent;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Tag(name = "Event Consumer 호출 테스트 API")
 public interface EventConsumerTestControllerSpec {
@@ -27,6 +29,20 @@ public interface EventConsumerTestControllerSpec {
     void comment(CommentEvent event);
 
 
+    @Operation(
+        requestBody = @RequestBody(
+            content = {
+                @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    examples = {
+                        @ExampleObject(name = "라이크 이벤트", value = LIKE_EVENT_PAYLOAD)
+                    }
+                )
+            }
+        )
+    )
+    void like(LikeEvent event);
+
     String COMMENT_EVENT_PAYLOAD = """
             {
             "type": "ADD",
@@ -35,4 +51,13 @@ public interface EventConsumerTestControllerSpec {
             "commentId" : 3
             }
         """;
+    String LIKE_EVENT_PAYLOAD = """
+            {
+            "type": "ADD",
+            "postId" : 1,
+            "userId" : 2,
+            "createdAt" : "2024-08-08T18:25:43.511Z"
+            }
+        """;
+
 }
